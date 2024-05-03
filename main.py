c14e901c1e9c56a954363e7a66ced8503be4e585
Garnier Tristan <126480368+Demonta0109@users.noreply.github.com>
2024-05-03 15:20:11 +0200

    ajout projectiles

--- main.py
+++ main.py
@@ -21,30 +21,58 @@ class Ship:
             self.y += self.speed
 
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, self.tilemap_coord[0], self.tilemap_coord[1], self.w, self.h, 0)
+        pyxel.blt(self.x, self.y, 0, self.tilemap_coord[0], self.tilemap_coord[1], self.w, self.h, 5)
 
+
+class Projectiles:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = 16
+        self.h = 16
+        self.speed = 4
+        self.tilemap_coord = [0, 104]
+
+    def update(self):
+        self.y -= self.speed
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, self.tilemap_coord[0], self.tilemap_coord[1], self.w, self.h, 5)
 
 class App:
     def __init__(self):
-        pyxel.init(256, 256, title='Hello Pyxel')
+        pyxel.init(256, 256, title='Hello Pyxel', fps=60)
         pyxel.load('1.pyxres')
+        self.projectiles = []
         self.ship = Ship(128, 128)
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
+
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.projectiles.append(Projectiles(self.ship.x, self.ship.y))
+
+
+        if self.projectiles:
+            for projectile in self.projectiles:
+                projectile.update()
+                if projectile.y < 0:
+                    self.projectiles.remove(projectile)
+
         self.ship.update()
 
     def draw(self):
-        pyxel.cls(5)
+        pyxel.cls(0)
 
         self.ship.draw()
+        for projectile in self.projectiles:
+            projectile.draw()
+
         #pyxel.blt(16, 16, 0, 0, 0, 16, 16, 0)
 
 if __name__ == '__main__':
     App()
-
-
-    print("testes")
